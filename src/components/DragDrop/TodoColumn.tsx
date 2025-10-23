8085a8c9c2d2a03c3fa1c0dcc620e3cfe44fd580
Truong62 <truong06022004@gmail.com>
2025-10-23 15:07:00 +0700

    fix minor

--- src/components/DragDrop/TodoColumn.tsx
+++ src/components/DragDrop/TodoColumn.tsx
@@ -135,7 +135,7 @@ export const TodoColumn: React.FC<TodoColumnProps> = ({columnType, todos, onDrop
               d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
             />
           </svg>
-          <p className="text-sm font-medium">Kéo và thả task vào đây</p>
+          <p className="text-sm font-medium">No tasks yet</p>
         </div>
       ) : (
         <ul
